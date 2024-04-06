e1620548f6a8e99c2a1f402a8329e5ab6e181096
Smajlli <amer.smajli@icloud.com>
2024-04-06 12:59:00 +0200

    Update component -> Get question value and add styles

--- components/Response.js
+++ components/Response.js
@@ -1,7 +1,8 @@
 import '../app/globals.css'
 
-function Response({answer}) {
+function Response({answer, question}) {
     return <div className='w-full'>
+        <p className='font-bold mb-4 text-cyan-600'>{question}</p>
         {answer}
     </div>
 }
