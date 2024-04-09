4b2daaa7826b502acf02935b450ead949442075e
Smajlli <amer.smajli@icloud.com>
2024-04-09 13:29:47 +0200

    Implement chat history storage with supabase

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,20 +1,42 @@
 import { NextResponse } from "next/server"
 import { CohereClient } from "cohere-ai";
-import { chatHistory } from "@/seeds";
+import { supabase } from "@/utils/supabase/supabase";
 
 const cohere = new CohereClient({token: process.env.COHERE_KEY});
 
 export async function POST(req) {
+    
+    const reqData = await req.formData();
 
-    const data = await req.formData();
+    const question = reqData.get('key');
+    const userId = reqData.get('userId');
 
-    const question = data.get('key');
+    let chatHistory = [];
 
-    chatHistory.push({role: "USER", message: question});
+    try {
+        await supabase.from('chat_history').insert({
+            role: "USER",
+            message: question,
+            user_id: userId
+        })
+    } catch(err) {
+        console.log(err)
+    }
+
+    try {
+        const {data} = await supabase.from('chat_history').select().eq('user_id', userId);
+        data.map((res) => {
+            return chatHistory.push({role: res.role, message: res.message});
+        })
+    } catch (err) {
+        console.log(err)
+    }
+    
+    console.log(chatHistory)
 
     async function response() {
         return cohere.chat({
-            chatHistory: chatHistory,
+            chatHistory,
             message: question,
             connectors: [{ id: 'web-search' }]
         })
