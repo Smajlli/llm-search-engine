c8dd875848347abfc1f85c72633f69f300def885
Smajlli <amer.smajli@icloud.com>
2024-04-10 14:00:51 +0200

    Update styles for login page

--- app/login/page.js
+++ app/login/page.js
@@ -4,7 +4,6 @@ import '@/app/globals.css'
 
 import { useState } from 'react';
 import { supabase } from '@/utils/supabase/supabase';
-import { redirect } from 'next/navigation';
 
 function Login() {
     const [email, setEmail] = useState('');
@@ -28,15 +27,22 @@ function Login() {
         } catch(err) {
             console.log(err)
         }
+        
     }
 
-    return <div>
-        <div>Log in</div>
-        <div>e-mail:</div>
-        <input type='text' name='email' value={email} onChange={handleEmail}/>
-        <div>password:</div>
-        <input type='password' name='password' value={password} onChange={handlePassword} />
-        <button onClick={handleLogin}>Log in</button>
+    return <div className='flex items-center justify-center'>
+        <div className='flex flex-col'>
+            <div className='text-center text-3xl font-bold my-8'>Log in</div>
+            <div className='flex flex-col'>
+                <label htmlFor='email'>E-mail:</label>
+                <input type='text' id='email' name='email' value={email} onChange={handleEmail} />
+            </div>
+            <div className='flex flex-col'>
+                <label htmlFor='password'>Password:</label>
+                <input type='password' id='password' name='password' value={password} onChange={handlePassword} />
+            </div>
+            <button className='p-2 mt-8 text-center border-solid border-2 border-orange-400 rounded-2xl hover:border-teal-200 duration-200' onClick={handleLogin}>Log in</button>
+        </div>
     </div>
 }
 
