85cbc10670d4b403e51c98654fc9bf108df98b4d
Smajlli <amer.smajli@icloud.com>
2024-04-15 14:56:41 +0200

    Check if there is a user and get user

--- app/page.js
+++ app/page.js
@@ -1,6 +1,20 @@
+'use client'
+
 import Link from "next/link";
+import { useState, useEffect } from "react";
+import { supabase } from "@/utils/supabase/supabase";
 
 export default function Home() {
+  const [profile, setProfile] = useState();
+
+  useEffect(() => {
+    async function getProfile() {
+      const {data: {user}} = await supabase.auth.getUser();
+      setProfile(user);
+    }
+    getProfile();
+  }, [])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 relative bg-slate-50">
       <div className="w-10/12 h-10/12 flex items-center justify-center absolute">
@@ -11,6 +25,7 @@ export default function Home() {
         <div className="flex flex-col justify-center items-center h-3/4 w-3/4">
           <div className="text-6xl font-semibold text-center z-10 drop-shadow-xl">Wellcome to LLM powered search engine</div>
           <Link href={'/chat'} className="p-2 mt-8 text-center border-solid border-2 border-orange-400 rounded-2xl hover:border-teal-200 duration-200">Get started</Link>
+          {profile ? <Link href={`/profile/${profile.id}`} className="p-2 mt-8 text-center border-solid border-2 border-orange-400 rounded-2xl hover:border-teal-200 duration-200">Profile</Link> : null}
         </div>
       </div>
     </main>
