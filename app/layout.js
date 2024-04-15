80c3c6322927fef82170c67dbaf2b0bbdb5b3be3
Smajlli <amer.smajli@icloud.com>
2024-04-15 14:55:28 +0200

    Temporary remove navigation

--- app/layout.js
+++ app/layout.js
@@ -13,10 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <navbar className="w-full flex items-center gap-3 h-11 bg-slate-100 ">
+        {/* <navbar className="w-full flex items-center gap-3 h-11 bg-slate-100 ">
            <Link href={'/chat'} className="hover:text-cyan-600 transition-all">Chat</Link> 
            <Link href={'/'} className="hover:text-cyan-600 transition-all">Home</Link>
-        </navbar>
+        </navbar> */}
         {children}
       </body>
     </html>
