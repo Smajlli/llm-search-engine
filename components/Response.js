9fb864c4ae4b7f4bb57743e139d7a78d5a7c4375
Smajlli <amer.smajli@icloud.com>
2024-04-15 14:59:26 +0200

    Update component -> Update styles

--- components/Response.js
+++ components/Response.js
@@ -1,7 +1,7 @@
 import '../app/globals.css'
 
 function Response({answer, question}) {
-    return <div className='w-full'>
+    return <div className='w-full mb-8'>
         <p className='font-bold mb-4 text-cyan-600'>{question}</p>
         {answer}
     </div>
