0d0544193c103e2d5fb3fcfce257ff58403266b4
Smajlli <amer.smajli@icloud.com>
2024-04-22 20:15:04 +0200

    Import EditAvatar component and create handler function for avatar. Temporary slice chat history

--- app/profile/[profileId]/page.js
+++ app/profile/[profileId]/page.js
@@ -3,10 +3,14 @@
 import '@/app/globals.css'
 import { useState, useEffect } from 'react';
 import { supabase } from '@/utils/supabase/supabase';
+import EditAvatar from '@/components/EditAvatar';
+import Image from 'next/image';
+
 
 function Profile({params}) {
     const [profile, setProfile] = useState();
     const [chatHistory, setChatHistory] = useState([]);
+    const [editAvatar, setEditAvatar] = useState(false);
 
     useEffect(() => {
         async function getProfile() {
@@ -21,10 +25,18 @@ function Profile({params}) {
         getChatHistory();
     }, [])
 
+    const handleAvatar = () => {
+        setEditAvatar(!editAvatar);
+    }
+
     if(!profile || profile.length === 0) {
         return null
     } else {
         return <div className='flex flex-col items-center justify-center my-8'>
+            <div onClick={handleAvatar} className='cursor-pointer'>
+                {profile[0].profile_image.length === 0 ? <div className='w-32 h-32 border-2 border-solid rounded-full flex justify-center items-center text-3xl mb-4'> + </div> : <Image src={profile[0].profile_image} width={150} height={150} /> }
+            </div>
+            {editAvatar ? <EditAvatar profile={profile[0]}/> : null}
             <div className='text-3xl font-bold'>
                 {profile[0].full_name}
             </div>
