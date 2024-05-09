b1645e196d2dd54fd16ca4ac9d26e94569c9b8bd
Smajlli <amer.smajli@icloud.com>
2024-05-09 19:20:56 +0200

    Create component for displaying uer message in chat

---
+++ components/UserMessage.js
@@ -0,0 +1,32 @@
+'use client'
+
+import '@/app/globals.css'
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import { supabase } from '@/utils/supabase/supabase';
+
+function UserMessage({question, userId}) {
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+        async function getUser() {
+            const {data} = await supabase.from('profiles').select('*').eq('id', userId)
+            setUser(data);
+        }
+        getUser();
+    }, [])
+
+    if(!user || user.length === 0) {
+        return null
+    } else {
+        return <div>
+            <div className='flex flex-row'>
+                <Image src={user[0].profile_image} width={20} height={20} className='mr-2' />
+                <div className='font-bold'> You </div>
+            </div>
+            <div className='mb-6'>{question}</div>
+        </div>
+    }
+}
+
+export default UserMessage;
