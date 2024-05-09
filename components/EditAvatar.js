ae6c7abed0c598a2d1a02bc57bc91e5a0490315b
Smajlli <amer.smajli@icloud.com>
2024-05-09 19:09:06 +0200

    Create loading effect with useState and react-spinners

--- components/EditAvatar.js
+++ components/EditAvatar.js
@@ -7,14 +7,17 @@ import { supabase } from '@/utils/supabase/supabase';
 import Image from 'next/image';
 import avatar from '@/public/Avatar1.svg'
 import axios from 'axios';
+import PulseLoader from 'react-spinners/PulseLoader'
 
 function EditAvatar({profile}) {
     const [avatarImg, setAvatarImg] = useState('');
+    const [loading, setLoading] = useState(false);
     const data = new FormData();
 
     const handleNewAvatar = async () => {
         data.append('user', profile.id);
-
+        setLoading(true);
+        setTimeout(() => setLoading(false), 1000)
         try {
             await axios({
                 method: 'POST',
@@ -36,7 +39,7 @@ function EditAvatar({profile}) {
 
     return <div className='flex flex-col items-center justify-evenly w-60 h-52 bg-white boder-solid border-2 rounded-lg absolute z-10'>
         <div className='flex justify-center items-center'>
-            {avatarImg ? <Image src={`/${avatarImg}.svg`} width={100} height={100} alt='profileImage' /> : <Image src={avatar} width={100} height={100} alt='profileImage' />}
+            {loading ? <div className='h-24 w-24 flex items-center justify-center'> <PulseLoader loading={loading} color={'#000000'} size={10} aria-label="Loading Spinner" data-testid="loader" /> </div> : avatarImg ? <Image src={`/${avatarImg}.svg`} width={100} height={100} alt='profileImage' /> : <Image src={avatar} width={100} height={100} alt='profileImage' />}
         </div>
         <button onClick={handleNewAvatar} className='p-2 border-solid border-2 rounded-lg hover:bg-slate-700 hover:text-white duration-200'>Generate random avatar</button>
         <button onClick={handleSaveAvatar} className='p-2 border-solid border-2 rounded-lg hover:bg-slate-700 hover:text-white duration-200'>Set as avatar</button>
