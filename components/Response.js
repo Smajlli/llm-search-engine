0b814fd80b9505b4db89278855a795e91ecf17ac
Smajlli <amer.smajli@icloud.com>
2024-05-09 19:19:54 +0200

    Update component -> Render UserMessage component

--- components/Response.js
+++ components/Response.js
@@ -1,9 +1,14 @@
 import '../app/globals.css'
+import UserMessage from './UserMessage';
 
-function Response({answer, question}) {
-    return <div className='w-full mb-8'>
-        <p className='font-bold mb-4 text-cyan-600'>{question}</p>
-        {answer}
+
+function Response({answer, question, userId}) {
+    return <div>
+        <UserMessage question={question} userId={userId}/>
+        <div className='w-full mb-8'>
+            <p className='font-bold mb-4 text-cyan-600'>Chat</p>
+            {answer}
+        </div>
     </div>
 }
 
