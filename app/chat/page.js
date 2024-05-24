8f533ca5610a660b6984d91ee6ee94b41d3d820e
Smajlli <amer.smajli@icloud.com>
2024-05-24 11:42:06 +0200

    Update component -> Define useRef for render tracking, make new conversation on initial render and update styles

--- app/chat/page.js
+++ app/chat/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import '../globals.css'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import Response from '@/components/Response';
 import ChatHistory from '@/components/ChatHistory';
@@ -16,6 +16,7 @@ function Chat() {
     const [isQuestion, setIsQuestion] = useState(false);
     const [loading, setLoading] = useState(false);
     const data = new FormData();
+    const renderCount = useRef(0);
 
     useEffect(() => {
          async function getSession() {
@@ -25,6 +26,19 @@ function Chat() {
         getSession();
     }, [])
 
+    useEffect(() => {
+        if(renderCount.current === 0) {
+            async function createConvo() {
+                const {data} = await supabase.from('conversations').insert({
+                    title: 'Random Title'
+                })
+            }
+            createConvo();
+            renderCount.current = renderCount.current + 1;
+        }
+    }, [])
+
+
     const handleChange = (e) => {
         setQuestion(e.target.value)
     }
@@ -32,6 +46,7 @@ function Chat() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setLoading(true);
+
         data.append('key', question);
         data.append('userId', user.id);
 
@@ -56,45 +71,48 @@ function Chat() {
     }, [answer])
 
     return <>
-        <div className='grid grid-cols-3 h-screen'>
+        <div className='w-screen h-screen relative flex'>
             {user ? <ChatHistory profileId={user.id} /> : null}
-            <div className=' w-full h-full flex flex-col flex-wrap justify-end col-span-2 col-end'>
-                {response.length || loading ? null : <div className='text-2xl font-bold mb-4 text-center h-2/4 w-5/6'> How can I help You today ? </div>}
-                {!response || response.length === 0 && isQuestion === false ? null :
-                    <div className='w-4/5'>
-                        {response.map(r => <Response answer={r.answer} question={r.question} userId={user.id} />)}
-                    </div>
+            <div className='w-full h-full flex flex-col items-center justify-between'>
+                {response.length || loading ? null : <div className='w-full h-full flex flex-col items-center justify-center'> <div className='text-2xl font-bold mb-4 text-center'> How can I help You today ? </div> </div>}
+                {
+                    !response || response.length === 0 && isQuestion === false ? null :
+                        <div className='w-full'>
+                            {response.map(r => <Response answer={r.answer} question={r.question} userId={user.id} />)}
+                        </div>
                 }
                 {loading ? <div className='mb-4'> <PulseLoader loading={loading} color={'#000000'} size={10} aria-label="Loading Spinner" data-testid="loader" /> </div> : null}
-                <form onSubmit={handleSubmit} className='mb-11 w-full text-center'>
-                    <div className='flex flex-row border-solid border-2 rounded-full w-5/6 justify-between pr-2'>
-                        <input type='text' className='border-none w-3/4 rounded-full' onChange={handleChange}></input>
-                        <button>
-                            <svg
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                                className='text-slate-500 h-9 w-9'
-                            >
-                                <path
-                                    d="M14.8285 11.9481L16.2427 10.5339L12 6.29122L7.7574 10.5339L9.17161 11.9481L11 10.1196V17.6568H13V10.1196L14.8285 11.9481Z"
-                                    fill="currentColor"
-                                />
-                                <path
-                                    fillRule="evenodd"
-                                    clipRule="evenodd"
-                                    d="M19.7782 4.22183C15.4824 -0.0739415 8.51759 -0.0739422 4.22183 4.22183C-0.0739415 8.51759 -0.0739422 15.4824 4.22183 19.7782C8.51759 24.0739 15.4824 24.0739 19.7782 19.7782C24.0739 15.4824 24.0739 8.51759 19.7782 4.22183ZM18.364 5.63604C14.8492 2.12132 9.15076 2.12132 5.63604 5.63604C2.12132 9.15076 2.12132 14.8492 5.63604 18.364C9.15076 21.8787 14.8492 21.8787 18.364 18.364C21.8787 14.8492 21.8787 9.15076 18.364 5.63604Z"
-                                    fill="currentColor"
-                                />
-                            </svg>
-                        </button>
-                    </div>
-                </form>
+                <div className='w-full h-32 text-center flex items-center justify-center'>
+                    <form onSubmit={handleSubmit} className='mb-11 w-9/12 text-center'>
+                        <div className='flex flex-row border-solid border-2 rounded-full w-5/6 justify-between pr-2'>
+                            <input type='text' className='border-none w-3/4 rounded-full' onChange={handleChange}></input>
+                            <button>
+                                <svg
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className='text-slate-500 h-9 w-9'
+                                >
+                                    <path
+                                        d="M14.8285 11.9481L16.2427 10.5339L12 6.29122L7.7574 10.5339L9.17161 11.9481L11 10.1196V17.6568H13V10.1196L14.8285 11.9481Z"
+                                        fill="currentColor"
+                                    />
+                                    <path
+                                        fillRule="evenodd"
+                                        clipRule="evenodd"
+                                        d="M19.7782 4.22183C15.4824 -0.0739415 8.51759 -0.0739422 4.22183 4.22183C-0.0739415 8.51759 -0.0739422 15.4824 4.22183 19.7782C8.51759 24.0739 15.4824 24.0739 19.7782 19.7782C24.0739 15.4824 24.0739 8.51759 19.7782 4.22183ZM18.364 5.63604C14.8492 2.12132 9.15076 2.12132 5.63604 5.63604C2.12132 9.15076 2.12132 14.8492 5.63604 18.364C9.15076 21.8787 14.8492 21.8787 18.364 18.364C21.8787 14.8492 21.8787 9.15076 18.364 5.63604Z"
+                                        fill="currentColor"
+                                    />
+                                </svg>
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </>
 }
 
-export default Chat;
+export default Chat; 
