bb9adc5bdc8a666bf215dd5a01a7a2fe524b4866
Smajlli <amer.smajli@icloud.com>
2024-05-31 10:56:29 +0200

    Update component -> Remove div

--- components/Response.js
+++ components/Response.js
@@ -3,7 +3,7 @@ import UserMessage from './UserMessage';
 
 
 function Response({answer, question, userId}) {
-    return <div>
+    return <div className='w-full flex flex-col'>
         <UserMessage question={question} userId={userId}/>
         <div className='w-full mb-8'>
             <p className='font-bold mb-4 text-cyan-600'>Chat</p>
