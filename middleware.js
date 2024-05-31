8d8d09c568b9c0e45941490771eb3226fbaf4ff5
Smajlli <amer.smajli@icloud.com>
2024-05-31 10:57:25 +0200

    Protect app routes by checking user session

--- middleware.js
+++ middleware.js
@@ -4,10 +4,15 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(req) {
     const res = NextResponse.next();
 
+    const allwaysAccessibleRoutes = ['http://localhost:3000/login', 'http://localhost:3000/signup']
+
     const supabase = createMiddlewareClient({req, res});
 
     const { data: {session} } = await supabase.auth.getSession();
 
+    if(!session && !allwaysAccessibleRoutes.includes(req.url)) {
+        return NextResponse.rewrite(new URL('http://localhost:3000/login'))
+    }
     return res;
 }
 
