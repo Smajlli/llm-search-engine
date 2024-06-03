f67c786056c22ea45bd6c3d628ef6258a59d700c
Smajlli <amer.smajli@icloud.com>
2024-06-03 16:29:03 +0200

    Update component -> Update styles

--- app/login/page.js
+++ app/login/page.js
@@ -5,6 +5,7 @@ import '@/app/globals.css'
 import { useState } from 'react';
 import { supabase } from '@/utils/supabase/supabase';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 function Login() {
     const [email, setEmail] = useState('');
@@ -32,18 +33,20 @@ function Login() {
         
     }
 
-    return <div className='flex items-center justify-center'>
+    return <div className='h-screen flex items-center justify-center items-center'>
         <div className='flex flex-col'>
             <div className='text-center text-3xl font-bold my-8'>Log in</div>
             <div className='flex flex-col'>
                 <label htmlFor='email'>E-mail:</label>
-                <input type='text' id='email' name='email' value={email} onChange={handleEmail} />
+                <input type='text' id='email' name='email' value={email} onChange={handleEmail} className='border-solid border-2 rounded-2xl'/>
             </div>
             <div className='flex flex-col'>
                 <label htmlFor='password'>Password:</label>
-                <input type='password' id='password' name='password' value={password} onChange={handlePassword} />
+                <input type='password' id='password' name='password' value={password} onChange={handlePassword} className='border-solid border-2 rounded-2xl' />
             </div>
-            <button className='p-2 mt-8 text-center border-solid border-2 border-orange-400 rounded-2xl hover:border-teal-200 duration-200' onClick={handleLogin}>Log in</button>
+            <button className='p-2 mt-8 text-center font-bold bg-slate-200 border-solid rounded-2xl hover:bg-slate-100 duration-200' onClick={handleLogin}>Log in</button>
+            <div className='text-center text-sm my-2'> Don't have account ? </div>
+            <Link href={'/signup'} className='text-center text-sm hover:underline'>Create account</Link>
         </div>
     </div>
 }
--- app/signup/page.js
+++ app/signup/page.js
@@ -3,6 +3,7 @@
 import '@/app/globals.css'
 import { useState } from 'react';
 import { supabase } from '@/utils/supabase/supabase';
+import Link from 'next/link';
 
 function Signup() {
     const [email, setEmail] = useState('');
@@ -41,27 +42,29 @@ function Signup() {
         });
     }
 
-    return <div className='flex justify-center content-center mt-8'>
+    return <div className='h-screen flex justify-center items-center mt-8'>
         <div className='flex flex-col'>
             <div className='text-center mb-8 text-3xl font-bold'>Sign Up</div>
-            <form onSubmit={handleSignup}>
+            <form onSubmit={handleSignup} className='flex flex-col justify-center'>
                 <div className='flex flex-col'>
                     <label htmlFor="fullname">Full name:</label>
-                    <input type='text' name='fullname' id='fullname' value={fullname} onChange={handleFullname} />
+                    <input type='text' name='fullname' id='fullname' value={fullname} onChange={handleFullname} className='border-solid rounded-2xl' />
                 </div>
                 <div className='flex flex-col'>
                     <label htmlFor="username">Username:</label>
-                    <input type='text' name='username' id='username' value={username} onChange={handleUsername} />
+                    <input type='text' name='username' id='username' value={username} onChange={handleUsername} className='border-solid rounded-2xl' />
                 </div>
                 <div className='flex flex-col'>
                     <label htmlFor="email">Email:</label>
-                    <input type='text' name='email' id='email' value={email} onChange={handleEmail} />
+                    <input type='text' name='email' id='email' value={email} onChange={handleEmail} className='border-solid rounded-2xl'/>
                 </div>
                 <div className='flex flex-col'>
                     <label htmlFor="password">Password:</label>
-                    <input type='password' name='password' id='password' value={password} onChange={handlePassword} />
+                    <input type='password' name='password' id='password' value={password} onChange={handlePassword} className='border-solid rounded-2xl' />
                 </div>
-                <button className="p-2 mt-8 text-center border-solid border-2 border-orange-400 rounded-2xl hover:border-teal-200 duration-200">Sign Up</button>
+                <button className="p-2 mt-8 text-center font-bold border-solid bg-slate-200 rounded-2xl hover:bg-slate-100 duration-200">Sign Up</button>
+                <div className='text-center text-sm my-2'>Already have account ?</div>
+                <Link href={'/login'} className='text-center text-sm hover:underline'>Log in</Link>
             </form>
         </div>
     </div>
