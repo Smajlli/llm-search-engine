c6f634f7ad3783c7c7817ad23ecc364dd1d0e55a
Smajlli <amer.smajli@icloud.com>
2024-06-12 12:29:08 +0200

    Update component -> Add functionality and make chat usable again

--- app/chat/[chatId]/page.js
+++ app/chat/[chatId]/page.js
@@ -1,15 +1,23 @@
 'use client'
 
 import '@/app/globals.css'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { supabase } from '@/utils/supabase/supabase';
 import Response from '@/components/Response';
 import ChatHistory from '@/components/ChatHistory';
 import PulseLoader from 'react-spinners/PulseLoader'
+import axios from 'axios';
 
 function Chat({params}) {
+    const [answer, setAnswer] = useState('');
+    const [question, setQuestion] = useState('');
+    const [response, setResponse] = useState([]);
+    const [isQuestion, setIsQuestion] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [conversation, setConversation] = useState([]);
     const [user, setUser] = useState();
+    const data = new FormData();
+    const responseCounter = useRef(0);
 
     useEffect(() => {
         async function getSession() {
@@ -27,17 +35,83 @@ function Chat({params}) {
         getConvo();
     }, [])
 
+    useEffect(() => {
+        if(conversation.length > 0) {
+            async function sendRequest() {
+                data.append('id', params.chatId)
+                await axios({
+                    method: 'POST',
+                    url: '/api/convo',
+                    data: data
+                }).then(res => setAnswer(res.data))
+            }
+            sendRequest();
+        }
+    }, [conversation])
+
+    useEffect(() => {
+        if (response.length > 0) {
+            async function createDialog() {
+                const { data, error } = await supabase.from('dialogs').insert({
+                    question: response[responseCounter.current].question,
+                    answer: response[responseCounter.current].answer,
+                    conversation_id: params.chatId
+                })
+                if (error) {
+                    console.log(error)
+                }
+            }
+            createDialog();
+            responseCounter.current = responseCounter.current + 1;
+        }
+    }, [response])
+
+    useEffect(() => {
+        if (answer && question) {
+            setResponse(curr => [...curr, { question, answer }]);
+        }
+    }, [answer])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        data.append('key', question);
+        data.append('userId', user.id);
+        data.append('convoId', params.chatId);
+
+        try {
+            await axios({
+                method: 'POST',
+                url: '/api/chat',
+                data: data
+            }).then(res => setAnswer(res.data));
+        } catch(err) {
+            console.log(err);
+        }
+
+        setIsQuestion(!isQuestion);
+        setLoading(false);
+    }
+
+    const handleChange = (e) => {
+        setQuestion(e.target.value)
+    }
+
+
     return <>
         <div className='w-screen h-screen relative flex'>
             {user ? <ChatHistory profileId={user.id} /> : null}
             <div className='w-full h-full flex flex-col items-center justify-between'>
                 <div className='w-full'>
                     {conversation.map(convo => <Response answer={convo.answer} question={convo.question} userId={user.id} />)}
+                    {!response || response.length === 0 && isQuestion === false ? null : response.map(res => <Response answer={res.answer} question={res.question} userId={user.id} />)}
+                    {loading ? <div className='mb-4'> <PulseLoader loading={loading} color={'#000000'} size={10} aria-label="Loading Spinner" data-testid="loader" /> </div> : null}
                 </div>
                 <div className='w-full h-32 text-center flex items-center justify-center'>
-                    <form className='mb-11 w-9/12 text-center'>
+                    <form onSubmit={handleSubmit} className='mb-11 w-9/12 text-center'>
                         <div className='flex flex-row border-solid border-2 rounded-full w-5/6 justify-between pr-2'>
-                            <input type='text' className='border-none w-3/4 rounded-full'></input>
+                            <input type='text' className='border-none w-3/4 rounded-full' onChange={handleChange}></input>
                             <button>
                                 <svg
                                     width="24"
