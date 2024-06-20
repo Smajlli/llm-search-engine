f930b72e91106f510b0965f43d179faf21942faf
Smajlli <amer.smajli@icloud.com>
2024-06-20 18:41:48 +0200

    Update component -> Edit styles and import inminite scroll functionality

--- app/chat/page.js
+++ app/chat/page.js
@@ -8,6 +8,7 @@ import ChatHistory from '@/components/ChatHistory';
 import { supabase } from '@/utils/supabase/supabase';
 import PulseLoader from 'react-spinners/PulseLoader'
 import { v4 as uuid } from 'uuid';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function Chat() {
     const [answer, setAnswer] = useState('');
@@ -120,7 +121,7 @@ function Chat() {
         }
 
         setIsQuestion(!isQuestion);
-        setLoading(false)
+        setLoading(false);
     }
 
     useEffect(() => {
@@ -130,45 +131,44 @@ function Chat() {
     }, [answer])
     
     return <>
-        <div className='w-screen h-screen relative flex'>
+        <div className='h-full w-full overflow-hidden flex'>
             {user ? <ChatHistory profileId={user.id} /> : null}
             <div className='w-full h-full flex flex-col items-center justify-between'>
-                {response.length || loading ? null : <div className='w-full h-full flex flex-col items-center justify-center'> <div className='text-2xl font-bold mb-4 text-center'> How can I help You today ? </div> </div>}
-                {
-                    !response || response.length === 0 && isQuestion === false ? null :
-                        <div className='w-full'>
-                            {response.map(r => <Response answer={r.answer} question={r.question} userId={user.id} />)}
-                        </div>
-                }
-                {loading ? <div className='mb-4'> <PulseLoader loading={loading} color={'#000000'} size={10} aria-label="Loading Spinner" data-testid="loader" /> </div> : null}
-                <div className='w-full h-32 text-center flex items-center justify-center'>
-                    <form onSubmit={handleSubmit} className='mb-11 w-9/12 text-center'>
-                        <div className='flex flex-row border-solid border-2 rounded-full w-5/6 justify-between pr-2'>
-                            <input type='text' className='border-none w-3/4 rounded-full' onChange={handleChange}></input>
-                            <button>
-                                <svg
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className='text-slate-500 h-9 w-9'
-                                >
-                                    <path
-                                        d="M14.8285 11.9481L16.2427 10.5339L12 6.29122L7.7574 10.5339L9.17161 11.9481L11 10.1196V17.6568H13V10.1196L14.8285 11.9481Z"
-                                        fill="currentColor"
-                                    />
-                                    <path
-                                        fillRule="evenodd"
-                                        clipRule="evenodd"
-                                        d="M19.7782 4.22183C15.4824 -0.0739415 8.51759 -0.0739422 4.22183 4.22183C-0.0739415 8.51759 -0.0739422 15.4824 4.22183 19.7782C8.51759 24.0739 15.4824 24.0739 19.7782 19.7782C24.0739 15.4824 24.0739 8.51759 19.7782 4.22183ZM18.364 5.63604C14.8492 2.12132 9.15076 2.12132 5.63604 5.63604C2.12132 9.15076 2.12132 14.8492 5.63604 18.364C9.15076 21.8787 14.8492 21.8787 18.364 18.364C21.8787 14.8492 21.8787 9.15076 18.364 5.63604Z"
-                                        fill="currentColor"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
-                    </form>
+                <div className='w-full h-full overflow-auto px-64 flex flex-col items-center'>
+                    <div>
+                        {!response || response.length === 0 && isQuestion === false ? null : response.map(res => <InfiniteScroll dataLength={response.length}> <Response answer={res.answer} question={res.question} userId={user.id} /> </InfiniteScroll>)}
+                        {loading ? <div className='mb-4'> <PulseLoader loading={loading} color={'#000000'} size={10} aria-label="Loading Spinner" data-testid="loader" /> </div> : null}
+                    </div>
+                    <div className='w-3/4 h-32 text-center flex flex-col items-center justify-center fixed bottom-0 bg-white'>
+                        <form onSubmit={handleSubmit} className='w-2/4 text-center'>
+                            <div className='flex flex-row border-solid border-2 rounded-full w-5/6 justify-between pr-2'>
+                                <input type='text' className='border-none w-3/4 rounded-full' onChange={handleChange}></input>
+                                <button>
+                                    <svg
+                                        width="24"
+                                        height="24"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className='text-slate-500 h-9 w-9'
+                                    >
+                                        <path
+                                            d="M14.8285 11.9481L16.2427 10.5339L12 6.29122L7.7574 10.5339L9.17161 11.9481L11 10.1196V17.6568H13V10.1196L14.8285 11.9481Z"
+                                            fill="currentColor"
+                                        />
+                                        <path
+                                            fillRule="evenodd"
+                                            clipRule="evenodd"
+                                            d="M19.7782 4.22183C15.4824 -0.0739415 8.51759 -0.0739422 4.22183 4.22183C-0.0739415 8.51759 -0.0739422 15.4824 4.22183 19.7782C8.51759 24.0739 15.4824 24.0739 19.7782 19.7782C24.0739 15.4824 24.0739 8.51759 19.7782 4.22183ZM18.364 5.63604C14.8492 2.12132 9.15076 2.12132 5.63604 5.63604C2.12132 9.15076 2.12132 14.8492 5.63604 18.364C9.15076 21.8787 14.8492 21.8787 18.364 18.364C21.8787 14.8492 21.8787 9.15076 18.364 5.63604Z"
+                                            fill="currentColor"
+                                        />
+                                    </svg>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
+
             </div>
         </div>
     </>
