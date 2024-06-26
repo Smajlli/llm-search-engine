e69f0b4cc6bc274598b685c8256cde420e2fed57
Smajlli <amer.smajli@icloud.com>
2024-06-26 10:17:55 +0200

    Create navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,53 @@
+import '@/app/globals.css'
+import Link from 'next/link';
+
+function Navbar({handleSidebar}) {
+
+    const sidebarOpenClose = () => {
+        handleSidebar();
+    }
+
+    return <div className='w-full h-10 flex flex-row justify-between items-center px-4 block sm:hidden'>
+        <div className='text-slate-500 cursor-pointer' onClick={sidebarOpenClose}>
+            <svg
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    d="M2 5.99519C2 5.44556 2.44556 5 2.99519 5H11.0048C11.5544 5 12 5.44556 12 5.99519C12 6.54482 11.5544 6.99039 11.0048 6.99039H2.99519C2.44556 6.99039 2 6.54482 2 5.99519Z"
+                    fill="currentColor"
+                />
+                <path
+                    d="M2 11.9998C2 11.4501 2.44556 11.0046 2.99519 11.0046H21.0048C21.5544 11.0046 22 11.4501 22 11.9998C22 12.5494 21.5544 12.9949 21.0048 12.9949H2.99519C2.44556 12.9949 2 12.5494 2 11.9998Z"
+                    fill="currentColor"
+                />
+                <path
+                    d="M2.99519 17.0096C2.44556 17.0096 2 17.4552 2 18.0048C2 18.5544 2.44556 19 2.99519 19H15.0048C15.5544 19 16 18.5544 16 18.0048C16 17.4552 15.5544 17.0096 15.0048 17.0096H2.99519Z"
+                    fill="currentColor"
+                />
+            </svg>
+        </div>
+        <div className='text-slate-500'>LLM Chat</div>
+        <Link href={'/chat'} className='text-slate-500 cursor-pointer'>
+            <svg
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M3 5C3 3.34315 4.34315 2 6 2H14C17.866 2 21 5.13401 21 9V19C21 20.6569 19.6569 22 18 22H6C4.34315 22 3 20.6569 3 19V5ZM13 4H6C5.44772 4 5 4.44772 5 5V19C5 19.5523 5.44772 20 6 20H18C18.5523 20 19 19.5523 19 19V9H13V4ZM18.584 7C17.9413 5.52906 16.6113 4.4271 15 4.10002V7H18.584Z"
+                    fill="currentColor"
+                />
+            </svg>
+        </Link>    
+    </div>
+}
+
+export default Navbar;
