83e89a71c570f8a74743210684b21c06b9b4c717
Smajlli <amer.smajli@icloud.com>
2024-06-29 14:42:50 +0200

    Change api model from Cohere to Groq and modify code

--- app/api/title/route.js
+++ app/api/title/route.js
@@ -1,21 +1,30 @@
 import { NextResponse } from "next/server";
-import { CohereClient } from "cohere-ai";
-import { supabase } from "@/utils/supabase/supabase";
+import Groq from "groq-sdk";
 
-const cohere = new CohereClient({token: process.env.COHERE_KEY});
+const groq = new Groq({apiKey: process.env.GROQ_KEY});
 
 export async function POST(req) {
 
     const reqData = await req.formData();
     const question = reqData.get('key');
 
+    async function chat() {
+        return groq.chat.completions.create({
+            messages: [
+                { role: 'user', content: `Can you only respond me with summarized title for this: ${question}, just title without "Sure" and without quotes` }
+            ],
+            model: 'llama3-8b-8192',
+            temperature: 0.5,
+            max_tokens: 1024,
+            stop: null,
+            stream: false,
+        });
+    };
+
     async function response() {
-        return cohere.chat({
-            message: `Can you only respond me with summarized title for this: ${question}, just title without "Sure" and without quotes`,
-            connectors: [{ id: 'web-search' }]
-        })
+        const chatCompletion = await chat();
+        return NextResponse.json(chatCompletion.choices[0]?.message?.content || "");
     }
 
-    return response().then(res => NextResponse.json(res.text));
-
+    return response();
 }
