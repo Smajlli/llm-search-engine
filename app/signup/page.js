a410cccae5658cf061e4bc0d4dbc3cb3da2fab7f
Smajlli <amer.smajli@icloud.com>
2024-07-03 14:03:29 +0200

    Update component -> Add dark mode styles

--- app/signup/page.js
+++ app/signup/page.js
@@ -46,40 +46,40 @@ function Signup() {
         router.push('/chat');
     }
 
-    return <div className='h-screen flex w-full h-full flex flex-col sm:flex-row'>
-        <div className='flex justify-center flex-col items-center bg-slate-100 h-full w-full hidden sm:flex'>
+    return <div className='h-screen flex w-full h-full flex flex-col sm:flex-row dark:bg-slate-900'>
+        <div className='flex justify-center flex-col items-center bg-slate-100 h-full w-full hidden sm:flex dark:bg-slate-800 dark:text-white'>
             <div className='text-6xl font-bold'>{logo} SSays</div>
             <div className='mt-6'>Large Laguange Model App</div>
             <div className='font-thin'>powered by Amer Smajlovic</div>
         </div>
-        <div className="flex justify-center flex-col items-center bg-slate-100 h-1/5 w-full sm:hidden">
+        <div className="flex justify-center flex-col items-center bg-slate-100 h-1/5 w-full sm:hidden dark:bg-slate-800 dark:text-white">
             <div className='text-4xl font-bold'>{logo} SSays</div>
             <div className='mt-6 text-xs'>Large Language Model App</div>
             <div className='font-thin text-xs'>powered by Amer Smajlovic</div>
         </div>
         <div className='flex flex-col h-full w-full items-center justify-center'>
             <div className='flex flex-col justify-center items-center'>
-                <div className='text-center mb-8 text-3xl font-bold'>Sign Up</div>
+                <div className='text-center mb-8 text-3xl font-bold dark:text-white'>Sign Up</div>
                 <form onSubmit={handleSignup} className='flex flex-col justify-center'>
                     <div className='flex flex-col'>
-                        <label htmlFor="fullname">Full name:</label>
+                        <label htmlFor="fullname" className='dark:text-white'>Full name:</label>
                         <input type='text' name='fullname' id='fullname' value={fullname} onChange={handleFullname} className='border-solid rounded-2xl' />
                     </div>
                     <div className='flex flex-col'>
-                        <label htmlFor="username">Username:</label>
+                        <label htmlFor="username" className='dark:text-white'>Username:</label>
                         <input type='text' name='username' id='username' value={username} onChange={handleUsername} className='border-solid rounded-2xl' />
                     </div>
                     <div className='flex flex-col'>
-                        <label htmlFor="email">Email:</label>
+                        <label htmlFor="email" className='dark:text-white'>Email:</label>
                         <input type='text' name='email' id='email' value={email} onChange={handleEmail} className='border-solid rounded-2xl' />
                     </div>
                     <div className='flex flex-col'>
-                        <label htmlFor="password">Password:</label>
+                        <label htmlFor="password" className='dark:text-white'>Password:</label>
                         <input type='password' name='password' id='password' value={password} onChange={handlePassword} className='border-solid rounded-2xl' />
                     </div>
-                    <button className="p-2 mt-8 text-center font-bold border-solid bg-slate-200 rounded-2xl hover:bg-slate-100 duration-200">Sign Up</button>
-                    <div className='text-center text-sm my-2'>Already have account ?</div>
-                    <Link href={'/login'} className='text-center text-sm hover:underline'>Log in</Link>
+                    <button className="p-2 mt-8 text-center font-bold border-solid bg-slate-200 rounded-2xl hover:bg-slate-100 duration-200 dark:bg-slate-600 dark:text-white dark:hover:bg-slate-500">Sign Up</button>
+                    <div className='text-center text-sm my-2 dark:text-white'>Already have account ?</div>
+                    <Link href={'/login'} className='text-center text-sm hover:underline dark:text-white'>Log in</Link>
                 </form>
             </div>
         </div> 
