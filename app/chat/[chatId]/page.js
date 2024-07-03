cca1c7fc1188db591aececa3a2ba027d87429739
Smajlli <amer.smajli@icloud.com>
2024-07-03 13:56:26 +0200

    Update component -> Enable toggling chat history and add dark mode styles

--- app/chat/[chatId]/page.js
+++ app/chat/[chatId]/page.js
@@ -12,6 +12,7 @@ import Cover from '@/components/Cover';
 import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 import Settings from '@/components/Settings';
+import ChatOptions from '@/components/ChatOptions';
 
 function Chat({params}) {
     const [answer, setAnswer] = useState('');
@@ -23,6 +24,8 @@ function Chat({params}) {
     const [user, setUser] = useState();
     const [settings, setSettings] = useState(false);
     const [sidebar, setSidebar] = useState(false);
+    const [chatHistory, setChatHistory] = useState(true);
+    const [darkMode, setDarkMode] = useState(false);
     const data = new FormData();
     const responseCounter = useRef(0);
     const textArea = useRef(null);
@@ -115,6 +118,14 @@ function Chat({params}) {
         setSidebar(!sidebar);
     }
 
+    const handleChatHistory = () => {
+        setChatHistory(!chatHistory);
+    }
+
+    const handleDarkMode = () => {
+        setDarkMode(!darkMode);
+    }
+
     useEffect(() => {
         textArea.current.style.height = "auto";
         textArea.current.style.height = textArea.current.scrollHeight + 'px';
@@ -122,44 +133,46 @@ function Chat({params}) {
 
     return <>
         {settings || sidebar ? <Cover /> : null}
-        {sidebar ? <Sidebar profileId={user.id} handleSettings={handleSettings} handleSidebar={handleSidebar} /> : null}
-        <Navbar  handleSidebar={handleSidebar}/>
-        <div className='h-full w-full overflow-hidden flex items-center justify-center'>
-            {settings ? <Settings profile={user} handleSettings={handleSettings} /> : null}
-            {user ? <ChatHistory profileId={user.id} handleSettings={handleSettings}/> : null}
-            <div className='w-full h-full flex flex-col justify-between'>
-                <div className='w-full h-full overflow-auto px-2 flex flex-col'>
-                    <div className='h-5/6 w-full overflow-auto my-8'>
-                        <InfiniteScroll dataLength={response.length} className='w-full xl:px-44'>
-                            {conversation.map(convo => <Response answer={convo.answer} question={convo.question} userId={user.id} />)}
-                            {!response || response.length === 0 && isQuestion === false ? null : response.map(res => <Response answer={res.answer} question={res.question} userId={user.id} />)}
-                            {loading ? <div className='mb-4'> <PulseLoader loading={loading} color={'#000000'} size={10} aria-label="Loading Spinner" data-testid="loader" /> </div> : null}
-                        </InfiniteScroll> 
-                    </div>   
-                    <div className='w-full h-12 sm:h-24 text-center flex flex-col items-center justify-center p-16 fixed bottom-0 bg-white relative z-20'>
-                        <form onSubmit={handleSubmit} className='w-full sm:w-2/4 text-center flex items-center justify-center'>
-                            <div className='flex flex-row border-solid rounded-full w-5/6 justify-between items-center pr-2 bg-gray-100 p-1.5 absolute'>
-                                <textarea className={`h-8 w-3/4 rounded-full bg-inherit border-transparent focus:border-transparent focus:ring-0 resize-none overflow-hidden box-border`} rows="1" onChange={handleChange} value={question} ref={textArea} placeholder='Ask SSays'>
-                                </textarea>
-                                <button className={`py-px px-2 rounded-full ${question.length > 0 ? 'bg-gray-500' : 'bg-gray-300'} duration-200 h-10 w-10 flex items-center justify-center`}>
-                                    <svg
-                                        width="18"
-                                        height="18"
-                                        viewBox="0 0 24 24"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className='text-white font-black'
-                                    >
-                                        <path
-                                            d="M17.6568 8.96219L16.2393 10.3731L12.9843 7.10285L12.9706 20.7079L10.9706 20.7059L10.9843 7.13806L7.75404 10.3532L6.34314 8.93572L12.0132 3.29211L17.6568 8.96219Z"
-                                            fill="currentColor"
-                                        />
-                                    </svg>
-                                </button>
-                            </div>
-                        </form>
+        <div className={`w-full h-full ${darkMode && 'dark'}`}>
+            {sidebar ? <Sidebar profileId={user.id} handleSettings={handleSettings} handleSidebar={handleSidebar} /> : null}
+            <Navbar handleSidebar={handleSidebar} />
+            <div className='h-full w-full overflow-hidden flex items-center justify-center dark:bg-slate-900'>
+                {settings ? <Settings profile={user} handleSettings={handleSettings} mode={handleDarkMode} /> : null}
+                {user && chatHistory ? <ChatHistory profileId={user.id} handleSettings={handleSettings} toggle={handleChatHistory}/> : <ChatOptions chatHistory={handleChatHistory}/>}
+                <div className='w-full h-full flex flex-col justify-between'>
+                    <div className='w-full h-full overflow-auto px-2 flex flex-col'>
+                        <div className='h-5/6 w-full overflow-auto my-8'>
+                            <InfiniteScroll dataLength={response.length} className='w-full xl:px-44'>
+                                {conversation.map(convo => <Response answer={convo.answer} question={convo.question} userId={user.id} />)}
+                                {!response || response.length === 0 && isQuestion === false ? null : response.map(res => <Response answer={res.answer} question={res.question} userId={user.id} />)}
+                                {loading ? <div className='mb-4'> <PulseLoader loading={loading} color={'#000000'} size={10} aria-label="Loading Spinner" data-testid="loader" /> </div> : null}
+                            </InfiniteScroll>
+                        </div>
+                        <div className='w-full h-12 sm:h-24 text-center flex flex-col items-center justify-center p-16 fixed bottom-0 bg-white relative z-20 dark:bg-slate-900'>
+                            <form onSubmit={handleSubmit} className='w-full sm:w-2/4 text-center flex items-center justify-center'>
+                                <div className='flex flex-row border-solid rounded-full w-5/6 justify-between items-center pr-2 bg-gray-100 p-1.5 dark:bg-slate-600 absolute'>
+                                    <textarea className={`h-8 w-3/4 rounded-full bg-inherit border-transparent focus:border-transparent focus:ring-0 resize-none overflow-hidden box-border`} rows="1" onChange={handleChange} value={question} ref={textArea} placeholder='Ask SSays'>
+                                    </textarea>
+                                    <button className={`py-px px-2 rounded-full ${question.length > 0 ? 'bg-gray-500' : 'bg-gray-400'} duration-200 h-10 w-10 flex items-center justify-center`}>
+                                        <svg
+                                            width="18"
+                                            height="18"
+                                            viewBox="0 0 24 24"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className='text-white font-black'
+                                        >
+                                            <path
+                                                d="M17.6568 8.96219L16.2393 10.3731L12.9843 7.10285L12.9706 20.7079L10.9706 20.7059L10.9843 7.13806L7.75404 10.3532L6.34314 8.93572L12.0132 3.29211L17.6568 8.96219Z"
+                                                fill="currentColor"
+                                            />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                </div>  
+                </div>
             </div>
         </div>
     </>
--- app/chat/page.js
+++ app/chat/page.js
@@ -13,6 +13,8 @@ import Settings from '@/components/Settings';
 import Cover from '@/components/Cover';
 import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
+import Link from 'next/link';
+import ChatOptions from '@/components/ChatOptions';
 
 function Chat() {
     const [answer, setAnswer] = useState('');
@@ -25,6 +27,8 @@ function Chat() {
     const [convoTitle, setConvoTitle] = useState('');
     const [settings, setSettings] = useState(false);
     const [sidebar, setSidebar] = useState(false);
+    const [chatHistory, setChatHistory] = useState(true);
+    const [darkMode, setDarkMode] = useState(false);
     const data = new FormData();
     const renderCount = useRef(0);
     const responseCounter = useRef(0);
@@ -145,6 +149,14 @@ function Chat() {
         setSidebar(!sidebar);
     }
 
+    const handleChatHistory = () => {
+        setChatHistory(!chatHistory);
+    }
+
+    const handleDarkMode = () => {
+        setDarkMode(!darkMode);
+    }
+
     useEffect(() => {
         if (answer && question) {
             setResponse(curr => [...curr, { question, answer }]);
@@ -158,44 +170,46 @@ function Chat() {
     
     return <>
         {settings || sidebar ? <Cover /> : null}
-        {sidebar ? <Sidebar profileId={user.id} handleSettings={handleSettings} handleSidebar={handleSidebar}/> : null}
-        <Navbar handleSidebar={handleSidebar}/>
-        <div className='h-dvh sm:h-full w-full overflow-hidden flex items-center justify-center'>
-            {settings ? <Settings profile={user} handleSettings={handleSettings} />  : null}
-            {user ? <ChatHistory profileId={user.id} handleSettings={handleSettings} today={date}/> : null}
-            <div className='w-full h-full flex flex-col justify-between'>
-                <div className='w-full h-full overflow-auto flex flex-col justify-center'>
-                    <div className='h-5/6 w-full overflow-auto my-8'>
-                        {response.length > 0 || loading ? <InfiniteScroll dataLength={response.length} className='w-full xl:px-44'>
-                            {!response || response.length === 0 && isQuestion === false ? null : response.map(res => <Response answer={res.answer} question={res.question} userId={user.id} />)}
-                            {loading ? <div className='mb-4 w-full h-full text-center flex flex-col justify-center items-center'> <PulseLoader loading={loading} color={'#000000'} size={10} aria-label="Loading Spinner" data-testid="loader" /> </div> : null}
-                        </InfiniteScroll> : <div className='w-full h-full text-center flex flex-col justify-center items-center'> 
-                                <div className='text-4xl font-bold'> {logo} SSays </div>
-                                <div className='mt-4'>How can I help you today ?</div>
-                        </div>}   
-                    </div>       
-                    <div className='w-full h-12 sm:h-24 text-center flex flex-col items-center justify-center p-16 fixed bottom-0 bg-white relative'>
-                        <form onSubmit={handleSubmit} className='w-full sm:w-2/4 text-center flex items-center justify-center'>
-                            <div className='flex flex-row border-solid rounded-full w-5/6 justify-between items-center pr-2 bg-gray-100 p-1.5 absolute'>
-                                <textarea className={`h-8 w-3/4 rounded-full bg-inherit border-transparent focus:border-transparent focus:ring-0 resize-none overflow-hidden box-border`} rows="1" onChange={handleChange} value={question} ref={textArea} placeholder='Ask SSays'>
-                                </textarea>
-                                <button className={`py-px px-2 rounded-full ${question.length > 0 ? 'bg-gray-500' : 'bg-gray-300'} duration-200 h-10 w-10 flex items-center justify-center`}>
-                                    <svg
-                                        width="18"
-                                        height="18"
-                                        viewBox="0 0 24 24"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className='text-white font-black'
-                                    >
-                                        <path
-                                            d="M17.6568 8.96219L16.2393 10.3731L12.9843 7.10285L12.9706 20.7079L10.9706 20.7059L10.9843 7.13806L7.75404 10.3532L6.34314 8.93572L12.0132 3.29211L17.6568 8.96219Z"
-                                            fill="currentColor"
-                                        />
-                                    </svg>
-                                </button>
-                            </div>
-                        </form>
+        <div className={`w-full h-full ${darkMode && 'dark'}`}>
+            {sidebar ? <Sidebar profileId={user.id} handleSettings={handleSettings} handleSidebar={handleSidebar} /> : null}
+            <Navbar handleSidebar={handleSidebar} />
+            <div className='h-dvh sm:h-full w-full overflow-hidden flex items-center justify-center dark:bg-slate-900'>
+                {settings ? <Settings profile={user} handleSettings={handleSettings} mode={handleDarkMode} /> : null}
+                {user && chatHistory ? <ChatHistory profileId={user.id} handleSettings={handleSettings} today={date} toggle={handleChatHistory}/> : <ChatOptions chatHistory={handleChatHistory}/>}
+                <div className='w-full h-full flex flex-col justify-between'>
+                    <div className='w-full h-full overflow-auto flex flex-col justify-center'>
+                        <div className='h-5/6 w-full overflow-auto my-8'>
+                            {response.length > 0 || loading ? <InfiniteScroll dataLength={response.length} className='w-full xl:px-44'>
+                                {!response || response.length === 0 && isQuestion === false ? null : response.map(res => <Response answer={res.answer} question={res.question} userId={user.id} />)}
+                                {loading ? <div className='mb-4 w-full h-full text-center flex flex-col justify-center items-center'> <PulseLoader loading={loading} color={'#000000'} size={10} aria-label="Loading Spinner" data-testid="loader" /> </div> : null}
+                            </InfiniteScroll> : <div className='w-full h-full text-center flex flex-col justify-center items-center'>
+                                <div className='text-4xl font-bold dark:text-white'> {logo} SSays </div>
+                                <div className='mt-4 dark:text-white'>How can I help you today ?</div>
+                            </div>}
+                        </div>
+                        <div className='w-full h-12 sm:h-24 text-center flex flex-col items-center justify-center p-16 fixed bottom-0 bg-white relative dark:bg-slate-900'>
+                            <form onSubmit={handleSubmit} className='w-full sm:w-2/4 text-center flex items-center justify-center'>
+                                <div className='flex flex-row border-solid rounded-full w-5/6 justify-between items-center pr-2 bg-gray-100 dark:bg-slate-600 p-1.5 absolute'>
+                                    <textarea className={`h-8 w-3/4 rounded-full bg-inherit border-transparent focus:border-transparent focus:ring-0 resize-none overflow-hidden box-border`} rows="1" onChange={handleChange} value={question} ref={textArea} placeholder='Ask SSays'>
+                                    </textarea>
+                                    <button className={`py-px px-2 rounded-full ${question.length > 0 ? 'bg-gray-500' : 'bg-gray-400'} duration-200 h-10 w-10 flex items-center justify-center`}>
+                                        <svg
+                                            width="18"
+                                            height="18"
+                                            viewBox="0 0 24 24"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className='text-white font-black'
+                                        >
+                                            <path
+                                                d="M17.6568 8.96219L16.2393 10.3731L12.9843 7.10285L12.9706 20.7079L10.9706 20.7059L10.9843 7.13806L7.75404 10.3532L6.34314 8.93572L12.0132 3.29211L17.6568 8.96219Z"
+                                                fill="currentColor"
+                                            />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
