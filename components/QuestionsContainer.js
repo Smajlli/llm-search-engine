da5371b73c83772810d3bd78661b633c89ff5dc6
Smajlli <amer.smajli@icloud.com>
2024-07-03 14:06:52 +0200

    Update component -> Add dark mode styles

--- components/QuestionsContainer.js
+++ components/QuestionsContainer.js
@@ -2,8 +2,8 @@ import '@/app/globals.css'
 import Question from './Question';
 
 function QuestionsContainer({questions, text}) {
-    return <div className='flex flex-col p-2 '>
-        <div className='text-xs font-bold text-slate-500 mb-2'>{text}</div>
+    return <div className='flex flex-col p-2'>
+        <div className='text-xs font-bold text-slate-500 mb-2 dark:text-white'>{text}</div>
         <div>{questions.map(q => <Question text={q.title} convoId={q.id}/>)}</div>
     </div>
 }
