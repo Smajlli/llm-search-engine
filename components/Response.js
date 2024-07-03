a2b2e7e4d0f56f311e07bbd99f668d0062e3ef4b
Smajlli <amer.smajli@icloud.com>
2024-07-03 14:07:06 +0200

    Update component -> Add dark mode styles

--- components/Response.js
+++ components/Response.js
@@ -5,8 +5,8 @@ import UserMessage from './UserMessage';
 function Response({answer, question, userId}) {
     return <div className='w-full flex flex-col'>
         <UserMessage question={question} userId={userId}/>
-        <div className='w-full mb-8'>
-            <p className='font-bold mb-4 text-cyan-600'>Chat</p>
+        <div className='w-full mb-8 dark:text-white'>
+            <p className='font-bold mb-2 text-slate-600 dark:text-slate-300'>SSays</p>
             {answer}
         </div>
     </div>
