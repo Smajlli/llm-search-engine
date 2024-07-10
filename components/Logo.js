0f3b52b115091b163f7679bb2e04d6115aa4ac15
Smajlli <amer.smajli@icloud.com>
2024-07-10 13:02:31 +0200

    Create component -> Create app logo

---
+++ components/Logo.js
@@ -0,0 +1,27 @@
+function Logo({width=150, height=150}) {
+    return <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 2480 3508" className="dark:text-white">
+        <path
+            d="M2061.28,1596.14S1707.55,2614.13,486.534,1975.43"
+            fill='none'
+            strokeLinecap='round'
+            strokeWidth={220}
+            stroke='#000'
+            strokeLinejoin='round'
+            fillRule='evenodd'
+        />
+        <path
+            d="M889.829,1278c98.943,0,179.151,79.92,179.151,178.51s-80.208,178.51-179.151,178.51-179.152-79.92-179.152-178.51S790.886,1278,889.829,1278Z"
+            strokeWidth={1}
+            fillRule='enenodd'
+        />
+        <path
+            id="Ellipse_1_copy"
+            data-name="Ellipse 1 copy"
+            d="M1399.36,1283.03c98.94,0,179.15,79.92,179.15,178.51s-80.21,178.51-179.15,178.51-179.15-79.92-179.15-178.51S1300.41,1283.03,1399.36,1283.03Z"
+            strokeWidth={1}
+            fillRule='enenodd'
+        />
+    </svg>
+}
+
+export default Logo;
