b3c6e97b78ec5e06ebd4ac96930e440c0dc9a4d4
Smajlli <amer.smajli@icloud.com>
2024-07-22 12:18:47 +0200

    Avoid optional types as possibly can

--- types/types.ts
+++ types/types.ts
@@ -1,40 +1,40 @@
 export type Answer = {
-    answer?: string,
-    links?: string[],
+    answer : string,
+    links : string[],
 }
 
 export type ResponseType = { 
-    question?: string, 
-    answer?: string, 
-    links?: string[]
+    question : string, 
+    answer : string, 
+    links : string[]
 }
 
 export type User = {
-    id?: string, 
-    email?: string, 
-    username?: string,
-    full_name?: string,
+    id? : string, 
+    email? : string, 
+    username? : string,
+    full_name? : string,
     profile_image?:string,
 }
 
-export type Conversations = {
-    id?: string,
-    title?: string, 
-    created_at?: string,
-    profile_id?: string,
+export type Conversation = {
+    id : string,
+    title : string, 
+    created_at : string,
+    profile_id : string,
 }
 
 export type ChatHistory = {
-    role: string, 
-    content: string,
+    role : string, 
+    content : string,
 }
 
 export type Logo = { value?: number }
 
 export type Dialog = {
-    id? : string,
-    question? : string,
-    answer? : string,
-    links? : string[],
-    conversation_id? : string,
+    id : string,
+    question : string,
+    answer : string,
+    links : string[],
+    conversation_id : string,
 }
